a885b172d66db9b38f6d3e3461c1a9ea791b161d
Nicoloso20100 <arquiel.nicolas@gmail.com>
2023-07-06 09:41:58 -0300

    Logica de mostrar tablas segun miembro

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const addButton = document.querySelector('.add-button')
 const formDiv = document.querySelector('.form-div')
 const partnerTable = document.querySelector('.partner-table')
-const accessTable = document.querySelector('.access-table')
+const accessTable = document.querySelectorAll('.access-table')
+const editButtons = document.querySelectorAll('.edit-button')
+const saveButtons = document.querySelectorAll('.save-button')
 
+console.log(accessTable)
 class Member {
     constructor(name, email, subscription, accredited, category) {
         this.name = name
@@ -20,54 +23,73 @@ const member3 = new Member('Saco Wea', 'saco.nicoolas@gmail.com', 7000, 'No', 'C
 const partners = [member1, member2, member3]
 
 
-partners.forEach(partner => {
+partners.forEach((partner, index) => {
     const row = partnerTable.insertRow() // Agrega una nueva fila a la tabla
-
+  
     // Agrega las celdas a la fila
     const nameCell = row.insertCell()
     nameCell.textContent = partner.name
-
+  
     const emailCell = row.insertCell()
     emailCell.textContent = partner.email
-
+  
     const subscriptionCell = row.insertCell()
     subscriptionCell.textContent = `$${partner.subscription}`
-
+  
     const accreditedCell = row.insertCell()
     accreditedCell.textContent = partner.accredited
-
+  
     const categoryCell = row.insertCell()
     categoryCell.textContent = partner.category
-
+  
     const actionCell = row.insertCell()
-    actionCell.innerHTML = `
-    <i class="bi bi-pencil-fill"></i>
-    <i class="bi bi-trash-fill"></i> `
-
-    row.addEventListener('click', () => {
-        console.log('Fila clickeada:', partner.name)
-        updateAccessTable(partner.category)
+    const infoButton = document.createElement('button')
+    infoButton.classList.add('info-button')
+    infoButton.setAttribute('data-index', index)
+    infoButton.innerHTML = `
+      <i class="bi bi-info-circle"></i>
+    `
+    actionCell.appendChild(infoButton)
+  
+    infoButton.addEventListener('click', (event) => {
+      event.stopPropagation() // Evitar la propagación del evento al hacer clic en el botón
+      const buttonIndex = event.currentTarget.getAttribute('data-index')
+      updateAccessTable(partner.category)
+      const partnerInfo = partners[buttonIndex]
+      console.log('Información del socio:', partnerInfo)
     })
-})
+  
+  })
 
+// CREAR BOTON PARA EDITAR, Y OTRO PARA VER LA INFO DE LOS PERMISOS DEL SOCIO
+// QUE NO SEA CLICKEAR LA ROW COMPLETA
 const updateAccessTable = (category) => {
     switch (category) {
         case 'Socio':
-            console.log('es socio')
+            console.log('es Socio ')
+            accessTable[0].classList.toggle('hidden')
+            accessTable[1].classList.add('hidden')
+            accessTable[2].classList.add('hidden')
             break
         case 'Adherente':
             console.log('es Adherente')
+            accessTable[1].classList.toggle('hidden')
+            accessTable[0].classList.add('hidden')
+            accessTable[2].classList.add('hidden')
             break
         case 'Cadete':
             console.log('es Cadete')
+            accessTable[2].classList.toggle('hidden')
+            accessTable[0].classList.add('hidden')
+            accessTable[1].classList.add('hidden')
             break
         default:
             break
     }
+
 }
 
+
 addButton.addEventListener('click', () => {
-    formDiv.classList.contains('hidden') ?
-        formDiv.classList.remove('hidden') :
-        formDiv.classList.add('hidden')
+    formDiv.classList.toggle('hidden')
 })
